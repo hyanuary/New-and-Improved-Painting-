cc6e96460672b423357c3d6d66502d5ee9cd85c2
SAE\hyanuary <1003495@student.sae.edu.au>
2017-10-17 18:31:45 +1000

    init commit for the new one have the canvas being broke down in different game object each canvas can be painted differently

---
+++ Assets/TexturePainter/Scripts/PaintDetection.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintDetection : MonoBehaviour {
+
+    public List<Camera> paintingCam = new List<Camera>();
+
+    public List<GameObject> brushContainer = new List<GameObject>();
+
+    public Camera sceneCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()    {
+
+        CanvasDetection();
+        brushContainer[0].SetActive(true);
+        brushContainer[1].SetActive(false);
+        if(Input.GetKey(KeyCode.A))
+        {
+            brushContainer[1].SetActive(true);
+            brushContainer[0].SetActive(false);
+        }
+    }
+
+    bool CanvasDetection()
+    {
+        RaycastHit hit;
+        Vector3 mousPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+        Ray cursorRay = sceneCamera.ScreenPointToRay(mousPos);
+
+        if (Physics.Raycast(cursorRay, out hit, 200))
+        {
+            MeshCollider meshCol = hit.collider as MeshCollider;
+
+            if (meshCol == null || meshCol.sharedMaterial == null)
+                return false;
+            if(meshCol.tag == "canvas2")
+            {
+                paintingCam[0].enabled = false;
+                paintingCam[1].enabled = true;
+                Debug.Log("canvas 2 activate");
+            }
+            else if(meshCol.tag == "canvas")
+            {
+                paintingCam[1].enabled = false;
+                paintingCam[0].enabled = true;
+            }
+
+            return true;
+        }
+        else
+            return false;
+
+    }
+}
